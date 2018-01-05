5d8e150d8d6cbcb966e3aa712898cb2bcc001164
Evgenii Zhdanovskiy <evgenii.zhdanovskiy@iqoption.com>
2018-01-05 11:48:41 +0300

    Add openBrowser

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,10 +1,13 @@
 package daemon
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"os"
+	"os/exec"
 	"os/signal"
+	"runtime"
 	"syscall"
 
 	"github.com/ezhdanovskiy/gocsvparser/db"
@@ -37,6 +40,14 @@ func Run(cfg *Config) error {
 
 	ui.Start(cfg.UI, m, l)
 
+	url := "http://" + cfg.ListenSpec
+	log.Printf("Open %s\n", url)
+	err = openBrowser(url)
+	if err != nil {
+		log.Printf("Error open %s: %v\n", url, err)
+		return err
+	}
+
 	waitForSignal()
 
 	return nil
@@ -47,4 +58,21 @@ func waitForSignal() {
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	s := <-ch
 	log.Printf("Got signal: %v, exiting.", s)
+}
+
+func openBrowser(url string) error {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+
+	return err
 }
